33cb8684d533079a1569afd08c03b5bd8ee4b8e9
Liêu Văn Học <22130089@st.hcmuaf.edu.vn>
2026-06-07 19:20:34 +0700

    Add user management API

---
+++ auto-parts-inspection-backend/src/main/java/com/tnd/auto_parts/user/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.tnd.auto_parts.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+    private Long id;
+    private String username;
+    private Set<String> roles;
+}
