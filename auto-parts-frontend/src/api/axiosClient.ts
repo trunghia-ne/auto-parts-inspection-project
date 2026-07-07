71c57b67c70c0c7181a8a8207b93a6a22deea740
Đặng Trung Nghĩa <138813617+trunghia-ne@users.noreply.github.com>
2026-07-07 20:23:07 +0700

    Update axiosClient.ts

--- auto-parts-frontend/src/api/axiosClient.ts
+++ auto-parts-frontend/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-   baseURL: import.meta.env.VITE_API_BASE_URL + '/api' || 'http://localhost:8080/api',
+   baseURL: 'https://tnd-auto-parts-api.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
